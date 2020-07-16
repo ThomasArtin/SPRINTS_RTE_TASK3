c6fd1d1178402cdbe0c00ced0915db176d91e00a
Thomas Joseph <1600440@eng.asu.edu.eg>
2020-07-16 18:31:49 +0200

    Add files via upload

--- Seat_Manager_Template.c
+++ Seat_Manager_Template.c
@@ -4,7 +4,7 @@
  * \brief Rte Component Template for AUTOSAR SWC: Seat_Manager
  *
  * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
- * Generated on 7/16/2020 06:25 PM
+ * Generated on 7/16/2020 05:30 PM
  *
  * For any inquiries: hassan.m.farahat@gmail.com
  *
@@ -26,14 +26,19 @@ void SeatManager_SetHeight (void)
 {
 	Std_ReturnType status;
 	MultiStateBtn HeightBtnState;
-	StepMotorStepType step;
-
 	/* Data Receive Points */
 	status = Rte_Read_rpSeatCtrlBtns_HeightBtnState(&HeightBtnState);
-	
-	/* Server Call Points */
-	status = Rte_Call_rpHeightMotor_Opr_Move(step);
-	
+	if (status == RTE_E_OK)
+	{
+		if (HeightBtnState == MULTI_STATE_BTN_MINUS)
+		{
+			 Rte_Call_rpHeightMotor_Move(MOTOR_STEP_MINUS);
+		}
+		else if (HeightBtnState == MULTI_STATE_BTN_PLUS)
+		{
+			 Rte_Call_rpHeightMotor_Move(MOTOR_STEP_PLUS);
+		}
+	}
 }
 
 
@@ -42,7 +47,7 @@ void SeatManager_SetHeight (void)
  * Runnable SeatManager_SetIncline
  *
  * Triggered By:
- *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_InclineBtnState
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_SlideBtnState
  *
  */
 
@@ -50,14 +55,20 @@ void SeatManager_SetIncline (void)
 {
 	Std_ReturnType status;
 	MultiStateBtn InclineBtnState;
-	StepMotorStepType step;
 
 	/* Data Receive Points */
 	status = Rte_Read_rpSeatCtrlBtns_InclineBtnState(&InclineBtnState);
-	
-	/* Server Call Points */
-	status = Rte_Call_rpSlideMotor_Opr_Move(step);
-	
+	if (status == RTE_E_OK)
+	{
+		if (InclineBtnState == MULTI_STATE_BTN_MINUS)
+		{
+			 Rte_Call_rpInclineMotor_Move(MOTOR_STEP_MINUS);
+		}
+		else if (InclineBtnState == MULTI_STATE_BTN_PLUS)
+		{
+			 Rte_Call_rpInclineMotor_Move(MOTOR_STEP_PLUS);
+		}
+	}
 }
 
 
@@ -74,13 +85,19 @@ void SeatManager_SetSlide (void)
 {
 	Std_ReturnType status;
 	MultiStateBtn SlideBtnState;
-	StepMotorStepType step;
 
 	/* Data Receive Points */
 	status = Rte_Read_rpSeatCtrlBtns_SlideBtnState(&SlideBtnState);
-	
-	/* Server Call Points */
-	status = Rte_Call_rpInclineMotor_Opr_Move(step);
-	
+	if (status == RTE_E_OK)
+	{
+		if (SlideBtnState == MULTI_STATE_BTN_MINUS)
+		{
+			 Rte_Call_rpSlideMotor_Move(MOTOR_STEP_MINUS);
+		}
+		else if (SlideBtnState == MULTI_STATE_BTN_PLUS)
+		{
+			 Rte_Call_rpSlideMotor_Move(MOTOR_STEP_PLUS);
+		}
+	}
 }
 
