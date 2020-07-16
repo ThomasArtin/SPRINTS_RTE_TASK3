c185403e8d398d4d2624eb36a131ca785945ce9a
Thomas Joseph <1600440@eng.asu.edu.eg>
2020-07-16 18:25:53 +0200

    Add files via upload

---
+++ Seat_Manager_Template.c
@@ -0,0 +1,86 @@
+/**
+ *
+ * \file Seat_Manager_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: Seat_Manager
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 7/16/2020 06:25 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_Seat_Manager.h"
+
+
+/**
+ *
+ * Runnable SeatManager_SetHeight
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_HeightBtnState
+ *
+ */
+
+void SeatManager_SetHeight (void)
+{
+	Std_ReturnType status;
+	MultiStateBtn HeightBtnState;
+	StepMotorStepType step;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_HeightBtnState(&HeightBtnState);
+	
+	/* Server Call Points */
+	status = Rte_Call_rpHeightMotor_Opr_Move(step);
+	
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetIncline
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_InclineBtnState
+ *
+ */
+
+void SeatManager_SetIncline (void)
+{
+	Std_ReturnType status;
+	MultiStateBtn InclineBtnState;
+	StepMotorStepType step;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_InclineBtnState(&InclineBtnState);
+	
+	/* Server Call Points */
+	status = Rte_Call_rpSlideMotor_Opr_Move(step);
+	
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetSlide
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_SlideBtnState
+ *
+ */
+
+void SeatManager_SetSlide (void)
+{
+	Std_ReturnType status;
+	MultiStateBtn SlideBtnState;
+	StepMotorStepType step;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_SlideBtnState(&SlideBtnState);
+	
+	/* Server Call Points */
+	status = Rte_Call_rpInclineMotor_Opr_Move(step);
+	
+}
+
